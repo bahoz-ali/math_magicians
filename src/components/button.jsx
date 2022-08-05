27a20883413ddfc47f6e659e1485728113fbd728
Bahoz <bahozali50@gmail.com>
2022-08-05 08:41:17 +0300

    convert the Button.jsx component from class based to function stateless based

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import './button_style.css';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Button = (props) => {
+  const { icon, theme, number, handleCalculate } = props;
+  const colorTheme = theme ? `btn ${theme}` : 'btn btn--secondary';
+  const numberClass = number ? `btn_container ${number}` : 'btn_container';
 
-  render() {
-    const { icon, theme, number, handleCalculate } = this.props;
-    const colorTheme = theme ? `btn ${theme}` : 'btn btn--secondary';
-    const numberClass = number ? `btn_container ${number}` : 'btn_container';
-
-    return (
-      <div className={numberClass}>
-        <button
-          onClick={handleCalculate}
-          name={icon}
-          type="button"
-          className={colorTheme}>
-          {icon}
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={numberClass}>
+      <button
+        onClick={handleCalculate}
+        name={icon}
+        type="button"
+        className={colorTheme}>
+        {icon}
+      </button>
+    </div>
+  );
+};
 
 export default Button;
