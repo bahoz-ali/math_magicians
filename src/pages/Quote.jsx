2df415c52edc86b23888800dfa7f777ef4b0eae2
Bahoz <bahozali50@gmail.com>
2022-08-10 21:42:27 +0300

    add style to the calculator css

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './quote_style.css'
 
 const Quote = () => {
-  return <div>Quote</div>;
+  return (
+    <section className='quote_section'>
+      <p>
+        Mathematics is not about the numbers, equations, computations, or
+        algorithms: its is about understanding.
+        <span className="author"> __William Paul Thurston</span>
+      </p>
+    </section>
+  );
 };
 
 export default Quote;
