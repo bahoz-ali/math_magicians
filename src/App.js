ea63a9bc8e1a611de538294fba7c294ef4e3a767
Bahoz <bahozali50@gmail.com>
2022-08-10 21:59:18 +0300

    fix linter errors, and update quote page style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './pages/Calculator';
 import Header from './components/Header';
-import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 
--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './quote_style.css'
+import './quote_style.css';
 
 const Quote = () => {
   return (
-    <section className='quote_section'>
+    <section className="quote_section">
       <p>
         Mathematics is not about the numbers, equations, computations, or
         algorithms: its is about understanding.
-        <span className="author"> __William Paul Thurston</span>
+        <br /> <span className="author"> _William Paul Thurston</span>
       </p>
     </section>
   );
